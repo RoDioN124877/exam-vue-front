db5f4e1bd2b847086585e72f7e2550acf79f39f8
Rodion <rodion.chubarov@mail.ru>
2024-02-27 16:49:27 +0600

    frst

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div v-for="skin in skins.data" :key="skin.id">
+      <p>{{ skin.price }}</p>
+      <img :src="skin.img" alt="" />
+      <p>{{ skin.title }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+const skins = ref<any>()
+
+onMounted(async () => {
+  const data = await fetch('http://localhost:8081/')
+  skins.value = await data.json()
+})
+</script>
+
+<style scoped></style>
